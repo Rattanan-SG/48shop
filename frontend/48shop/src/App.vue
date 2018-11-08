e8692c418c943632c317131f7286c95e5f235ed7
Gulfz <flog-fy@hotmail.com>
2018-11-08 20:54:18 +0700

    [ADD] components to App.vue

--- frontend/48shop/src/App.vue
+++ frontend/48shop/src/App.vue
@@ -1,30 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <nav class="navbar" id="nav-top" role="navigation" aria-label="main navigation">
+      <div class="container" id="nav-box">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="./assets/logo2.png" width="200" height="100">
+          </a>
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container" id="home-box">
+      <h1>{{ msg }}</h1>
+    </div>
+    
   </div>
 </template>
 
 <script>
+import './../node_modules/bulma/css/bulma.css';
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to 48 Shop'
     }
   }
 }
@@ -37,24 +56,32 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
+html {
+  background: #F2F2F2;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+nav {
+  background: #F2F2F2;
+  border-bottom: 10px;
+  border-color: #E0E0E0;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+#nav-top {
+  border-bottom: 10px;
+  border-color: #E0E0E0;
 }
 
-a {
-  color: #42b983;
+#nav-box {
+  width: 1000px;
+}
+
+#home-box {
+  background: white;
+  width: 1000px;
+  min-height: 2000px;
+  margin-top: 13px;
+  box-shadow: 2px 10px grey;
 }
 </style>
