08f633a9604b84d8b9142a24a2ae3b5dd1f2a906
Mee <wutthipong.mee@mail.kmutt.ac.th>
2018-11-09 23:34:42 +0700

    [ADD]AddressService

---
+++ backend/src/main/java/com/sit/int202/backend/Address/AddressService.java
@@ -0,0 +1,10 @@
+package main.java.com.sit.int202.backend.Address;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+class AddressService {
+    @Autowired
+    private AddressRepository addressRepository;
+}
