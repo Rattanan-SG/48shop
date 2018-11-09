94a2804585d66e5c71dcbe94fa1087c192b63eb7
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2018-11-09 16:58:21 +0700

    [ADD] Order repository

---
+++ backend/src/main/java/com/sit/int202/backend/Order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.sit.int202.backend.Order;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
