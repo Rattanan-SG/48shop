78e13323805abc485f81f1532bef9c1a544b527c
Mee <wutthipong.mee@mail.kmutt.ac.th>
2018-11-09 23:33:58 +0700

    [ADD]AddressRepository

---
+++ backend/src/main/java/com/sit/int202/backend/Address/AddressRepository.java
@@ -0,0 +1,10 @@
+package main.java.com.sit.int202.backend.Address;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface AddressRepository extends JpaRepository<Address, Long>{
+
+}
