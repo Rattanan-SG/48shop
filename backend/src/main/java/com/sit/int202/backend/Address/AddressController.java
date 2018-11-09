b95eba5fa6228f265b01c5bb38d32e718c0b1175
Mee <wutthipong.mee@mail.kmutt.ac.th>
2018-11-09 23:33:17 +0700

    [ADD]AddressController

---
+++ backend/src/main/java/com/sit/int202/backend/Address/AddressController.java
@@ -0,0 +1,18 @@
+package main.java.com.sit.int202.backend.Address;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class AddressController {
+    @Autowired
+    private AddressService addressCardService;
+
+
+   
+    
+}
