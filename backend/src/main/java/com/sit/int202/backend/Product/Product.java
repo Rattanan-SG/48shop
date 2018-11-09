da85f194c16ab151ae38069c4d85e66fc45957bb
BounceGem <mildsadz@gmail.com>
2018-11-09 18:54:20 +0700

    [UPDATE] product model minor changed

--- backend/src/main/java/com/sit/int202/backend/Product/Product.java
+++ backend/src/main/java/com/sit/int202/backend/Product/Product.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Entity
-@Table(name="creditcards")
+@Table(name="product")
 public class Product implements Serializable{
 
     @Id
