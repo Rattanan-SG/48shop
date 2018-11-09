c7ee312635b07cb3708ac8a4d2fb8fbc6ec9afbc
Rattanan-SG <rattanan.nan@mail.kmutt.ac.th>
2018-11-09 16:58:43 +0700

    [ADD] Order controller

---
+++ backend/src/main/java/com/sit/int202/backend/Order/OrderController.java
@@ -0,0 +1,12 @@
+package com.sit.int202.backend.Order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+    
+}
