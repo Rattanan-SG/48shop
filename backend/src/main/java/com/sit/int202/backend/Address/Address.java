6dc9ec51cadf5e6cb5c737ee2ffa88895f1f39ab
Mee <wutthipong.mee@mail.kmutt.ac.th>
2018-11-10 22:32:09 +0700

    [UPDATE]Address Model

--- backend/src/main/java/com/sit/int202/backend/Address/Address.java
+++ backend/src/main/java/com/sit/int202/backend/Address/Address.java
@@ -1,3 +1,84 @@
 package main.java.com.sit.int202.backend.Address;
 
+import java.io.Serializable;
 
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="address")
+public class Address implements Serializable{
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String detail;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String district;
+
+    @NotBlank
+    private int zipcode;
+
+
+
+   
+    public long getId() {
+        return id;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+   
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+   
+    public String getCity() {
+        return city;
+    }
+
+   
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+   
+    public String getDistrict() {
+        return district;
+    }
+
+   
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    
+    public int getZipcode() {
+        return zipcode;
+    }
+
+   
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+}
