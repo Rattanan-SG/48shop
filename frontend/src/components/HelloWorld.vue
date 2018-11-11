b8cedb50e8982a828a434e7b1fc543baaa70dc01
Gulfz <flog-fy@hotmail.com>
2018-11-11 16:48:48 +0700

    [ADD] ProductDetail.vue

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,94 +1,102 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <nav class="navbar" id="nav-top" role="navigation" aria-label="main navigation">
+      <div class="container" id="nav-box">
+        <div class="navbar-brand">
+          <a class="navbar-item">
+            <img src="./../assets/logo.png" width="200" height="100">
+          </a>
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container" id="home-box">
+      <h1>{{ msg }}</h1>
+      <div class="columns">
+        <div class="column">
+          <div class="card">
+            <div class="card-image is-4by3">
+              <img src="./../assets/logo.png" alt="Placeholder image">
+            </div>
+            <div class="card-content">
+              product name
+            </div>
+            <div class="card-footer">
+              <router-link to="/ProductDetail"><div class="button">Buy now</div></router-link>
+            </div>
+          </div>
+        </div>
+         <div class="column">
+          <div class="card">
+            <div class="card-image is-4by3">
+              <img src="./../assets/logo.png" alt="Placeholder image">
+            </div>
+            <div class="card-content">
+              product name
+            </div>
+            <div class="card-footer">
+              <router-link to="/ProductDetail"><div class="button">Buy now</div></router-link>
+            </div>
+          </div>
+        </div>
+         <div class="column">
+          <div class="card">
+            <div class="card-image is-4by3">
+              <img src="./../assets/logo.png" alt="Placeholder image">
+            </div>
+            <div class="card-content">
+              product name
+            </div>
+            <div class="card-footer">
+              <router-link to="/ProductDetail"><div class="button">Buy now</div></router-link>
+            </div>
+          </div>
+        </div>
+         <div class="column">
+          <div class="card">
+            <div class="card-image is-4by3">
+              <img src="./../assets/logo.png" alt="Placeholder image">
+            </div>
+            <div class="card-content">
+              product name
+            </div>
+            <div class="card-footer">
+              <router-link to="/ProductDetail"><div class="button">Buy now</div></router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import './../../node_modules/bulma/css/bulma.css';
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'หน้าแรก สำหรับแสดงสินค้า'
     }
   }
 }
@@ -96,18 +104,24 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+nav {
+  border-bottom: 10px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#nav-top {
+  border-bottom: 10px;
+  border-color: #E0E0E0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#nav-box {
+  width: 1000px;
 }
-a {
-  color: #42b983;
+
+#home-box {
+  background: white;
+  width: 1000px;
+  min-height: 2000px;
+  margin-top: 13px;
+  box-shadow: 2px 10px grey;
 }
 </style>
---
+++ frontend/src/components/ProductDetail.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ProductDetail">
+    <div class="container" id="home-box">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import './../../node_modules/bulma/css/bulma.css';
+
+export default {
+  name: 'ProductDetail',
+  data () {
+    return {
+      msg: 'This is Detail page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  border-bottom: 10px;
+}
+
+#nav-top {
+  border-bottom: 10px;
+  border-color: #E0E0E0;
+}
+
+#nav-box {
+  width: 1000px;
+}
+
+#home-box {
+  background: white;
+  width: 1000px;
+  min-height: 2000px;
+  margin-top: 13px;
+  box-shadow: 2px 10px grey;
+}
+</style>
