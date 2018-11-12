a60c33a13abbb5de82894a8f723530fe5a5ce741
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-12 10:33:30 +0700

    [ADD] UserProfile Service

---
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/UserProfileService.java
@@ -0,0 +1,10 @@
+package com.sit.int202.backend.UserProfile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileService {
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+}
