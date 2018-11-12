87772e29c9cc80015572a311f5a3040a700ed155
StyxK <nosomething050@gmail.com>
2018-11-12 23:06:07 +0700

    [ADD] CreditCard model (for charge)

---
+++ backend/src/main/java/com/sit/int202/backend/Order/CreditCard.java
@@ -0,0 +1,50 @@
+package com.sit.int202.backend.Order;
+
+public class CreditCard(){
+    
+    private String cardId;
+    private int expM;
+    private int expY;
+    private String name;
+    private String address;
+    private String zip;
+
+    public void setCardId(String cardId){
+        this.cardId = cardId;
+    }
+    public void setExpM(int expM){
+        this.expM = expM;
+    }
+    public void setExpY(int expY){
+        this.expY = expY;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public void setZip(String zip){
+        this.zip = zip;
+    }
+
+    public String getCardId(){
+        return cardId;
+    }
+    public int getExpM(){
+        return expM;
+    }
+    public int getExpY(){
+        return expY;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getZip(){
+        return zip;
+    }
+
+}
