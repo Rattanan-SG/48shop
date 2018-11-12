543c568168112765851362a45e0f0163275d8451
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-12 10:37:03 +0700

    [DELETE] CreditCard Service
    use omise to store credit card instead

--- backend/src/main/java/com/sit/int202/backend/CreditCard/CreditCardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sit.int202.backend.CreditCard;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface CreditCardRepository extends JpaRepository<CreditCard, Long>{
-
-}
