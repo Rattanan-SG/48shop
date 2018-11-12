de212a64579cd6bec45a6996667f3a1039199ecc
StyxK <nosomething050@gmail.com>
2018-11-12 20:23:39 +0700

    [ADD] method getToken and charge on OrderService
    รอ implement ให้ส่ง amount กับ credit card (JSON) ด้วย RequestBody พร้อมกันได้

--- backend/src/main/java/com/sit/int202/backend/Order/OrderController.java
+++ backend/src/main/java/com/sit/int202/backend/Order/OrderController.java
@@ -1,12 +1,30 @@
 package com.sit.int202.backend.Order;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import co.omise.ClientException;
+import co.omise.models.OmiseException;
+import co.omise.models.Token;
 
 @RestController
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @PostMapping("/Charge")
+    public void charge(@RequestBody Object creditCard) throws ClientException, IOException, OmiseException{
+        Token token = orderService.getToken(creditCard);
+        orderService.charge(100000/* example price*/,token); // need to implement this method
+    }
     
 }
--- backend/src/main/java/com/sit/int202/backend/Order/OrderService.java
+++ backend/src/main/java/com/sit/int202/backend/Order/OrderService.java
@@ -1,13 +1,33 @@
 package com.sit.int202.backend.Order;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.omise.Client;
+import co.omise.ClientException;
+import co.omise.models.Card;
+import co.omise.models.Charge;
+import co.omise.models.OmiseException;
+import co.omise.models.Token;
+
 @Service
 public class OrderService {
+
+    private static final String OMISE_OPUBLIC_KEY = "pkey_test_5dviz6scp4tdk4cm0au";
+    private static final String OMISE_SECRET_KEY = "skey_test_5dviz7b84hcwu90jews";
+    private static final String TH_BAHT = "thb";
 
     @Autowired
     private OrderRepository orderRepository;
@@ -23,5 +43,32 @@ public class OrderService {
     
     public Order save(Order order){
         return orderRepository.save(order);
+    }
+
+    public Token getToken(Object data) throws ClientException, IOException, OmiseException{
+        Client client = new Client(OMISE_OPUBLIC_KEY,OMISE_SECRET_KEY);
+        LinkedHashMap paymentData = (LinkedHashMap) data;
+        System.out.print(data);
+        Token token = client.tokens().create(
+            new Token.Create().card(
+                new Card.Create()
+                .number((String)paymentData.get("card_id"))
+                .expirationMonth((int)paymentData.get("exp_m"))
+                .expirationYear((int)paymentData.get("exp_y"))
+                .securityCode((String)paymentData.get("cvv"))
+                .name((String)paymentData.get("name"))
+                .city((String)paymentData.get("address"))
+                .postalCode((String)paymentData.get("zip"))
+            )
+        );
+        return token;
+    }
+
+    public void charge(long amount,Token token) throws ClientException, IOException, OmiseException {
+        Client client = new Client(OMISE_OPUBLIC_KEY,OMISE_SECRET_KEY);
+        Charge charge = client.charges().create(
+            new Charge.Create()
+            .amount(amount).currency(TH_BAHT).card(token.getId())
+        );
     }
 }
