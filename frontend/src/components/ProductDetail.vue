2bd06a82b22d4a5adeed9d32593d863ef6d4860e
Gulfz <flog-fy@hotmail.com>
2018-11-13 17:11:59 +0700

    [ADD] function to open/close modal in ProductDetail.vue

--- frontend/src/components/ProductDetail.vue
+++ frontend/src/components/ProductDetail.vue
@@ -1,82 +1,82 @@
 <template>
-  <div class="ProductDetail">
-    <div class="container" id="home-box">
-      <h1>{{ msg }}</h1>
-      <div class="columns">
-        <div class="column is-one-fifth">
-            <img src="./../assets/item1.png" alt="Placeholder image">
-            item1
-        </div>
-        <div class="column">
-            <div class="card">
-                <div class="card-header">
-                    ITEM1
+    <div class="ProductDetail">
+        <div class="container" id="home-box">
+            <h1>{{ msg }}</h1>
+            <div class="columns">
+                <div class="column is-one-fifth">
+                    <img src="./../assets/item1.png" alt="Placeholder image">
+                    item1
                 </div>
-                <div class="card-content">
-                    kgkdfghdfjghdglghdfd;gjsg
-                </div>
-                <div class="card-footer">
-                    <router-link to="/Home"><div class="button">Back</div></router-link>
-                    <router-link to="/ProductDetail"><div class="button" disabled>Buy now</div></router-link>
-                    <router-link to="/CategoryProduct"><div class="button">this</div></router-link>
+                <div class="column">
+                    <div class="card">
+                        <div class="card-header">
+                            ITEM1
+                        </div>
+                        <div class="card-content">
+                            kgkdfghdfjghdglghdfd;gjsg
+                        </div>
+                        <div class="card-footer">
+                            <router-link to="/Home"><div class="button">Back</div></router-link>
+                            <router-link to="/ProductDetail"><div class="button" disabled>Buy now</div></router-link>
+                            <router-link to="/CategoryProduct"><div class="button">this</div></router-link>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
-      </div>
-    </div>
-    <div class="navbar is-fixed-bottom" id="nav-bot">
-        <div class="container" id="nav-bot-box">
-            เพิ่มเครดิต
-            <div class="button" @click="showLoginModal=true" id="button">เพิ่มเครดิต</div>
-            <div class="button" @click="showLoginModal=true" id="button2">เพิ่มที่อยู่</div>
         </div>
-    </div>
-      <!-- popup Cradit -->
-    
-      <div class="modal-card" id="popUpCradit">
-          <div class="container" >
-              <div class="modal" id="page-modal">
-                  <div class="modal-background"></div>
-                  <section class="modal-card-body">
-                      <div class="modal-content" id="screen">
-                          <header id ="head" class="modal-card-head">
-                              <p class="modal-card-title" style="text-align: center;">ใส่บัตรเครดิต/เดบิต</p>
-                                  <button class="delete"  aria-label="close"></button>
-                          </header>
-                        <div class="columns is-multiline" style=" display: flex;" id="bodyPopUp">
-                            <div class="field is-grouped" id="number">
-                                <label  id="text" class="label">หมายเลขบัตร</label>
-                                <label> <img src="../assets/visa.svg" width="20" height="10" style="  margin-left: 10px;"></label>
-                                <label> <img src="../assets/mastercard.svg" width="20" height="10" style="  margin-left: 10px;"></label>
-                            </div>
-                            <div class="control">
-                                <input class="input" type="text" placeholder="Ex. 1234567890123456">
-                            </div>
-                            <div class="field" id="name" style=" margin-top: 13px;">
-                                <label class="label" id="text">ชื่อที่ปรากฏบนบัตร</label>
+        <div class="navbar is-fixed-bottom" id="nav-bot">
+            <div class="container" id="nav-bot-box">
+                เพิ่มเครดิต
+                <div class="button" @click="showCreditModal" id="button">เพิ่มเครดิต</div>
+                <div class="button" @click="showAddressModal" id="button2">เพิ่มที่อยู่</div>
+            </div>
+        </div>
+        <!-- popup Cradit -->
+        
+        <div class="modal-card" id="popUpCradit">
+            <div class="container" >
+                <div class="modal" id="page-modal" v-bind:style="{ display: showCredit}">
+                    <div class="modal-background"></div>
+                    <section class="modal-card-body">
+                        <div class="modal-content" id="screen">
+                            <header id ="head" class="modal-card-head">
+                                <p class="modal-card-title" style="text-align: center;">ใส่บัตรเครดิต/เดบิต</p>
+                                <button class="delete"  aria-label="close" @click="closeCreditModal"></button>
+                            </header>
+                            <div class="columns is-multiline" style=" display: flex;" id="bodyPopUp">
+                                <div class="field is-grouped" id="number">
+                                    <label  id="text" class="label">หมายเลขบัตร</label>
+                                    <label> <img src="../assets/visa.svg" width="20" height="10" style="  margin-left: 10px;"></label>
+                                    <label> <img src="../assets/mastercard.svg" width="20" height="10" style="  margin-left: 10px;"></label>
+                                </div>
+                                <div class="control">
+                                    <input class="input" type="text" placeholder="Ex. 1234567890123456">
+                                </div>
+                                <div class="field" id="name" style=" margin-top: 13px;">
+                                    <label class="label" id="text">ชื่อที่ปรากฏบนบัตร</label>
                                     <div class="control">
                                         <input class="input" type="text" placeholder="Ex. โสภณ จำปาซ่อนกลิ่น">
                                     </div>
-                            </div>
-                            <div class="field" id="province">
-                                <label id="text" class="label">จังหวัด</label>
-                                <div class="control">
-                                    <input class="input" type="text" placeholder="Ex. สมุทรสงคราม">
-                                  </div>
-                            </div>
+                                </div>
+                                <div class="field" id="province">
+                                    <label id="text" class="label">จังหวัด</label>
+                                    <div class="control">
+                                        <input class="input" type="text" placeholder="Ex. สมุทรสงคราม">
+                                    </div>
+                                </div>
                                 <div class="field is-grouped" id="MY">
-                                  <div class="field" id="month">
-                                      <label  id="text" class="label">เดือนหมดอายุ</label>
-                                      <div class="control ">
-                                          <input id="box" class="input" type="text" placeholder="mm" style="width: 70px;  margin-right: 48px;">
-                                      </div>
-                                  </div>
-                                  <div class="field" id="year">
-                                      <label  id="text" class="label">ปีหมดอายุ</label>
-                                      <div class="control">
-                                          <input id="box" class="input" type="text" placeholder="yyyy" style="width: 70px;">
-                                      </div>
-                                  </div>
+                                <div class="field" id="month">
+                                    <label  id="text" class="label">เดือนหมดอายุ</label>
+                                    <div class="control ">
+                                        <input id="box" class="input" type="text" placeholder="mm" style="width: 70px;  margin-right: 48px;">
+                                    </div>
+                                </div>
+                                <div class="field" id="year">
+                                    <label  id="text" class="label">ปีหมดอายุ</label>
+                                    <div class="control">
+                                        <input id="box" class="input" type="text" placeholder="yyyy" style="width: 70px;">
+                                    </div>
+                                </div>
                                 </div>    
                                 <div class="field" id="CVV">
                                     <label  id="text" class="label ">รหัสรักษาความปลอดภัย</label>
@@ -102,23 +102,23 @@
                                         <button class="button is-success">บันทึก</button>
                                     </footer>
                                 </div>
-                          </div>    
-                      </div>
-                  </section>
-              </div>
-          </div>
-      </div>
+                            </div>    
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
 
-        <!-- popupAddress -->
-      <div class="modal-card" id="popUpAddress">
-          <div class="container" >
-              <div class="modal" id="page-modal2" >
-                <div class="modal-background"></div>
+            <!-- popupAddress -->
+        <div class="modal-card" id="popUpAddress">
+            <div class="container" >
+                <div class="modal" id="page-modal2" v-bind:style="{ display: showAddress}">
+                    <div class="modal-background"></div>
                     <section class="modal-card-body">
                         <div class="modal-content" id="screenAddress">
                             <header id ="head" class="modal-card-head">
                                 <p class="modal-card-title">ส่งสินค้าไปที่</p>
-                                <button class="delete"  aria-label="close"></button>
+                                <button class="delete"  aria-label="close" @click="closeAddressModal"></button>
                             </header>
                             <div class="columns is-multiline" style=" display: flex;" id="bodyPopUpAddress">
                                 <div class="field is-horizontal" id="addressbox">
@@ -135,38 +135,36 @@
                                         </div>
                                     </div>
                                 </div>
-                                    <div class="field" id="addressbox-line2">
-                                        <label class="label">ที่อยู่ในการจัดส่ง</label>
-                                        <div class="control">
-                                            <textarea class="textarea" id="address" placeholder="Ex. เช่น 112/44 ตึกหอสมุด ด้านใน KMUTT ถนนสุขสบายดี ตำบลมะละกอ" style="height: 100px; width: 300px;"></textarea>
-                                        </div>
+                                <div class="field" id="addressbox-line2">
+                                    <label class="label">ที่อยู่ในการจัดส่ง</label>
+                                    <div class="control">
+                                        <textarea class="textarea" id="address" placeholder="Ex. เช่น 112/44 ตึกหอสมุด ด้านใน KMUTT ถนนสุขสบายดี ตำบลมะละกอ" style="height: 100px; width: 300px;"></textarea>
                                     </div>
-                                    <div class="field" style="margin-left: 400px; margin-top: 100px;">
-                                        <label class="label">จังหวัด</label>
-                                        <div class="control">
-                                            <input id="province" class="input" type="text" placeholder="Ex. กรุงเทพมหานคร " style="width: 300px; height: 50px;">
-                                        </div>
+                                </div>
+                                <div class="field" style="margin-left: 400px; margin-top: 100px;">
+                                    <label class="label">จังหวัด</label>
+                                    <div class="control">
+                                        <input id="province" class="input" type="text" placeholder="Ex. กรุงเทพมหานคร " style="width: 300px; height: 50px;">
                                     </div>
-                                    <div class="field" style="margin-left: 400px; margin-bottom: 2000px;">
-                                        <label class="label">หมายเลขไปรษณีย์</label>
-                                        <div class="control">
-                                            <input id="postcode" class="input" type="text" placeholder="Ex. 10140 " style="width: 300px; height: 50px;">
-                                        </div>
+                                </div>
+                                <div class="field" style="margin-left: 400px; margin-bottom: 2000px;">
+                                    <label class="label">หมายเลขไปรษณีย์</label>
+                                    <div class="control">
+                                        <input id="postcode" class="input" type="text" placeholder="Ex. 10140 " style="width: 300px; height: 50px;">
                                     </div>
-                                        
+                                </div>          
                             </div>
                             <div class="field is-grouped" id="buttonAddress">
-                                    <footer id= "footer" class="modal-card-foot" >
-                                        <button class="button" style="width: 100px;">ยกเลิก</button>
-                                        <button class="button is-success" style="width: 100px; margin-left: 10px;">บันทึก</button>
-                                    </footer>
-                                </div>
-                    </div>
-                  </section>
-                  
-              </div>
-          </div>
-      </div>
+                                <footer id= "footer" class="modal-card-foot" >
+                                    <button class="button" style="width: 100px;">ยกเลิก</button>
+                                    <button class="button is-success" style="width: 100px; margin-left: 10px;">บันทึก</button>
+                                </footer>
+                            </div>
+                        </div>
+                    </section> 
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -174,12 +172,28 @@
 import './../../node_modules/bulma/css/bulma.css';
 
 export default {
-  name: 'ProductDetail',
-  data () {
-    return {
-      msg: 'This is Detail page'
+    name: 'ProductDetail',
+    data () {
+        return {
+            msg: 'This is Detail page',
+            showCredit: '',
+            showAddress: ''
+        }
+    },
+    methods:{
+        showCreditModal: function(){
+            this.showCredit = 'block';
+        },
+        closeCreditModal: function(){
+            this.showCredit = '';
+        },
+        showAddressModal: function(){
+            this.showAddress = 'block';
+        },
+        closeAddressModal: function(){
+            this.showAddress = '';
+        }
     }
-  }
 }
 </script>
 
