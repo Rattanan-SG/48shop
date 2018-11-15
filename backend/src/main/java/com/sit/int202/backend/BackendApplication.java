70dd74207a8f428597688e435f7b79685cc745f2
StyxK <nosomething050@gmail.com>
2018-11-15 20:03:19 +0700

    [ADD] Spring-boot Security

--- backend/src/main/java/com/sit/int202/backend/BackendApplication.java
+++ backend/src/main/java/com/sit/int202/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.sit.int202.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableOAuth2Sso
 public class BackendApplication {
 
 	public static void main(String[] args) {
