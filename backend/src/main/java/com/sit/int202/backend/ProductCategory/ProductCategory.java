187c74da9622c8cc7633781e22e0252387c30f7e
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-11-15 17:21:29 +0700

    [ADD] ProductCategory Model + Map Set of Products

---
+++ backend/src/main/java/com/sit/int202/backend/ProductCategory/ProductCategory.java
@@ -0,0 +1,62 @@
+package com.sit.int202.backend.ProductCategory;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sit.int202.backend.Product.Product;
+
+@Entity
+@Table(name = "product_categories")
+public class ProductCategory implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(name = "name", nullable = false)
+    private String categoryName;
+
+    @OneToMany(fetch = FetchType.LAZY,
+    cascade = {
+        CascadeType.PERSIST,
+        CascadeType.MERGE
+    },
+    mappedBy = "product_categories")
+    @JsonIgnore
+    private Set<Product> product = new HashSet<>();
+
+    public ProductCategory() {
+
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+}
