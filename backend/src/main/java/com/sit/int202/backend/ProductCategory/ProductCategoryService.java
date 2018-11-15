a16f17e9f025a7b40349c68046e422938a2eac62
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-11-15 16:50:12 +0700

    [ADD] ProductCategoryService CRUD

---
+++ backend/src/main/java/com/sit/int202/backend/ProductCategory/ProductCategoryService.java
@@ -0,0 +1,35 @@
+package com.sit.int202.backend.ProductCategory;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductCategoryService {
+
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+
+    public List<ProductCategory> getProductCategoryList() {
+        List<ProductCategory> productCategoryList = productCategoryRepository.findAll();
+        return productCategoryList;
+    }
+
+    public ProductCategory getProductCategory(long category_id) {
+        ProductCategory productCategory = productCategoryRepository.getOne(category_id);
+        return productCategory;
+    }
+
+    public ProductCategory create(ProductCategory productCategory) {
+        ProductCategory productCategory_return = productCategoryRepository.save(productCategory);
+        return productCategory_return;
+    }
+
+    public long delete(long productCategory_id) {
+        productCategoryRepository.deleteById(productCategory_id);
+        return productCategory_id;
+
+    }
+
+}
