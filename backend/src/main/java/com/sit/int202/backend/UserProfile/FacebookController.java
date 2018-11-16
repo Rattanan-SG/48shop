6ef3d77eca046cefa95810e9f3427531af3c4015
StyxK <nosomething050@gmail.com>
2018-11-16 21:41:32 +0700

    [ADD] retrieve fb info form spring principal
    *need more implementation

--- backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
@@ -1,7 +1,17 @@
 package com.sit.int202.backend.UserProfile;
 
+import java.security.Principal;
+import java.util.LinkedHashMap;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FacebookController {
 
-
+    @GetMapping("/user")
+    public Principal user(Principal principal){
+        System.out.print("");
+        return principal;
+    }
 }
