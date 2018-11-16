5a5d7a485744d3c699cf8ea49abaabae2df1ec95
Tanapat Choochot <tanapat.pm@mail.kmutt.ac.th>
2018-11-16 22:07:07 +0700

    [UPDATE] add CrossOrigin

--- backend/src/main/java/com/sit/int202/backend/ProductCategory/ProductCategoryController.java
+++ backend/src/main/java/com/sit/int202/backend/ProductCategory/ProductCategoryController.java
@@ -2,14 +2,21 @@ package com.sit.int202.backend.ProductCategory;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ProductCategoryController {
 
     @Autowired
@@ -25,5 +32,17 @@ public class ProductCategoryController {
     public ResponseEntity<ProductCategory> getProductCategory(@PathVariable("product_id") long category_id){
         ProductCategory productCategory = productCategoryService.getProductCategory(category_id);
         return new ResponseEntity<>(productCategory, HttpStatus.OK);
+    }
+
+    @PostMapping("/category")
+    public ResponseEntity<ProductCategory> createProductCategory(@Valid @RequestBody ProductCategory productCategory){
+        ProductCategory productCategory_return = productCategoryService.create(productCategory);
+        return new ResponseEntity<>(productCategory_return, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/category/{category_id}")
+    public ResponseEntity<ProductCategory> updateProductCategory(@Valid @PathVariable String category_id, @RequestBody ProductCategory productCategory) {
+        ProductCategory productCategory_return = productCategoryService.create(productCategory);
+        return new ResponseEntity<>(productCategory_return, HttpStatus.OK);
     }
 }
