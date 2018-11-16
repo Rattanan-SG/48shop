b8b9b05fdbde7b89a2dd7017d8c3ef61cce9bf78
StyxK <nosomething050@gmail.com>
2018-11-16 16:07:46 +0700

    [ADD] login configure in facebookController

--- backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
@@ -1,5 +1,19 @@
 package com.sit.int202.backend.UserProfile;
 
-public class FacebookController{
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+public class FacebookController extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http
+        .csrf().disable()
+        .antMatcher("/**")
+        .authorizeRequests()
+            .antMatchers("/creditcard","/login","/webjars/**").permitAll()
+        // .anyRequest()
+        //     .authenticated()
+        .and().logout().logoutSuccessUrl("/").permitAll();
+    }
 }
