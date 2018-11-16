fa2c4841975c7e909b96291b9fcc66b3fd6c6342
StyxK <nosomething050@gmail.com>
2018-11-16 21:00:00 +0700

    [UPDATE] override spring security configure
    login only /login and /creditcard

--- backend/src/main/java/com/sit/int202/backend/BackendApplication.java
+++ backend/src/main/java/com/sit/int202/backend/BackendApplication.java
@@ -4,14 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableOAuth2Sso
 
-public class BackendApplication{
+public class BackendApplication extends WebSecurityConfigurerAdapter{
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	@Override
+    protected void configure (HttpSecurity http) throws Exception {
+        http
+        .csrf().disable().antMatcher("/**")
+        .authorizeRequests()
+			.antMatchers("/creditcard").authenticated()
+			.anyRequest().permitAll()
+
+            .and().logout().logoutSuccessUrl("/").permitAll();
+    }
 }
--- backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
@@ -1,19 +1,7 @@
 package com.sit.int202.backend.UserProfile;
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class FacebookController extends WebSecurityConfigurerAdapter{
+public class FacebookController {
 
-    @Override
-    protected void configure (HttpSecurity http) throws Exception {
-        http
-        .csrf().disable()
-        .antMatcher("/**")
-        .authorizeRequests()
-            .antMatchers("/creditcard","/login","/webjars/**").permitAll()
-        // .anyRequest()
-        //     .authenticated()
-        .and().logout().logoutSuccessUrl("/").permitAll();
-    }
+
 }
