be229e57cde4f1e68db7db2b7e3c755d67ff5830
StyxK <nosomething050@gmail.com>
2018-11-16 15:01:04 +0700

    [ADD] application.yml & @enbleOAuth (facebook)

--- backend/src/main/java/com/sit/int202/backend/BackendApplication.java
+++ backend/src/main/java/com/sit/int202/backend/BackendApplication.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableOAuth2Sso
-public class BackendApplication {
+
+public class BackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
