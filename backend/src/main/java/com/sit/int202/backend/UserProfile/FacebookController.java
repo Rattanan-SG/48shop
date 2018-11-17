24bd37f88c3e40483135ae3dd9135be92a5ea0cf
StyxK <nosomething050@gmail.com>
2018-11-17 12:27:25 +0700

    [ADD] restFB dependency

--- backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
@@ -1,8 +1,13 @@
 package com.sit.int202.backend.UserProfile;
 
+import java.lang.reflect.Field;
 import java.security.Principal;
 import java.util.LinkedHashMap;
 
+import com.restfb.FacebookClient;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class FacebookController {
 
     @GetMapping("/user")
-    public Principal user(Principal principal){
-        System.out.print("");
-        return principal;
+    public Object user() {
+        Object data = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        System.out.print(data);
+        return data;
     }
 }
