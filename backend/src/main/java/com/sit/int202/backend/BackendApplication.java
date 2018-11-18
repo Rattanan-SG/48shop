defcd57c2eb617e642efb5e50ac5621d55f56500
StyxK <nosomething050@gmail.com>
2018-11-18 21:58:49 +0700

    [DELETE] goodbye spring-secure & restFB

--- backend/src/main/java/com/sit/int202/backend/BackendApplication.java
+++ backend/src/main/java/com/sit/int202/backend/BackendApplication.java
@@ -1,51 +1,15 @@
 package com.sit.int202.backend;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableOAuth2Sso
-@Configuration
 
-public class BackendApplication extends WebSecurityConfigurerAdapter{
+public class BackendApplication{
 
 	public static void main(String[] args){
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-	@Override
-    protected void configure (HttpSecurity http) throws Exception {
-        http.cors().and()
-        .csrf().disable().antMatcher("/**")
-        .authorizeRequests()
-            .antMatchers("/login").authenticated()
-            .anyRequest().permitAll()
-
-            .and().logout().logoutSuccessUrl("/").permitAll();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 }
