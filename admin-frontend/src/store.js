e6a633d8d6ed3878a4ae55578f68d1f67d3da0ec
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-18 18:23:56 +0700

    [UPDATE] admin-frontend / vuex add store for addRecProduct

--- admin-frontend/src/store.js
+++ admin-frontend/src/store.js
@@ -5,21 +5,42 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    navbar: false
+    navbar: false,
+    recommendProducts: [],
+    products: []
   },
   mutations: {
     setNavBar: (state, show) => {
       state.navbar = show
+    },
+    setRecommendProducts: (state, product) => {
+      let tempRecommendProducts = state.recommendProducts.push(product)
+      state.recommendProducts = tempRecommendProducts
+    },
+    setProducts: (state, products) => {
+      state.products = products
     }
   },
   actions: {
     setNavBar: ({ commit }, show) => {
       commit('setNavBar', show)
+    },
+    setRecommendProducts: ({ commit }, product) => {
+      commit('setRecommendProducts', product)
+    },
+    setProducts: ({ commit }, products) => {
+      commit('setProducts', products)
     }
   },
   getters: {
     getNavBar: (state) => {
       return state.navbar
+    },
+    getRecommendProducts: (state) => {
+      return state.recommendProducts
+    },
+    getProducts: (state) => {
+      return state.products
     }
   }
 })
