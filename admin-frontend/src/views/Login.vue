7494b192e0a2e5f125fc52c9a0def1851fc2cc2b
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-18 14:37:37 +0700

    [ADD] admin-frontend / login page

---
+++ admin-frontend/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Admin Login</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <router-link to='/add-product'>
+                  <v-btn color="primary" @click='setNavBar(true)'>Login</v-btn>
+                </router-link>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  data: () => ({
+    drawer: null
+  }),
+  props: {
+    source: String
+  },
+  methods: {
+    ...mapActions([
+      'setNavBar'
+    ])
+  },
+  mounted () {
+    this.setNavBar(false)
+  }
+}
+</script>
