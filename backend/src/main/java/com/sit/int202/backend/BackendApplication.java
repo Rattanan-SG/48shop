fd569dedb22d61c4b2deb27d658ba7215cf65d8f
StyxK <nosomething050@gmail.com>
2018-11-18 13:34:59 +0700

    [UPDATE] /fbinfo & Configure cors

--- backend/src/main/java/com/sit/int202/backend/BackendApplication.java
+++ backend/src/main/java/com/sit/int202/backend/BackendApplication.java
@@ -1,16 +1,24 @@
 package com.sit.int202.backend;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableOAuth2Sso
+@Configuration
 
 public class BackendApplication extends WebSecurityConfigurerAdapter{
 
@@ -20,19 +28,24 @@ public class BackendApplication extends WebSecurityConfigurerAdapter{
 
 	@Override
     protected void configure (HttpSecurity http) throws Exception {
-        final CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-
-        http
+        http.cors().and()
         .csrf().disable().antMatcher("/**")
         .authorizeRequests()
-			.antMatchers("/creditcard","/login").authenticated()
-			.anyRequest().permitAll()
+            .antMatchers("/login").authenticated()
+            .anyRequest().permitAll()
 
             .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/FacebookController.java
@@ -6,6 +6,8 @@ import com.restfb.json.JsonObject;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -18,8 +20,7 @@ public class FacebookController {
     @Autowired
     FacebookService facebookService;
 
-    @GetMapping("/")
-
+    @GetMapping("/fbinfo")
     public LinkedHashMap getInfo(OAuth2Authentication authentication){
         OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
         String token = details.getTokenValue();
