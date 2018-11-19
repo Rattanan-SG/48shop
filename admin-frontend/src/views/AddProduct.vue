542efaca551fb985d0b4235fcd783664398a5f4b
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-19 22:26:49 +0700

    [UPDATE] admin-frontend / before add product need to confirmation

--- admin-frontend/src/views/AddProduct.vue
+++ admin-frontend/src/views/AddProduct.vue
@@ -9,6 +9,7 @@
                 <v-text-field
                   label="Name"
                   :rules='[rules.counter, rules.required]'
+                  v-model='name'
                   counter='255'
                   required
                 ></v-text-field>
@@ -17,6 +18,7 @@
                 <v-textarea
                   label="Detail"
                   counter='255'
+                  v-model='detail'
                   :rules='[rules.counter]'
                   required
                 ></v-textarea>
@@ -24,7 +26,7 @@
             </v-layout>
           </v-flex>
           <v-flex offset-md2 md6>
-            <img v-bind:src='imageUrl' alt='image' style='height: 250px' v-show="imageUrl.length != 0"/>
+            <img v-bind:src='img_url' alt='image' style='height: 250px' v-show="img_url.length != 0"/>
           </v-flex>
         </v-layout>
       </v-layout>
@@ -32,7 +34,7 @@
         <v-flex md5>
           <v-text-field
             label="Image Url"
-            v-model='imageUrl'
+            v-model='img_url'
             :rules='[rules.required]'
             required
           ></v-text-field>
@@ -43,6 +45,7 @@
           <v-text-field
             :rules='[rules.required, rules.number]'
             label="Price"
+            v-model='price'
             required
           ></v-text-field>
         </v-flex>
@@ -50,41 +53,97 @@
       <v-layout>
         <v-flex md3>
           <v-overflow-btn
-            :items="categoryList"
+            :items="getCategories"
             label="Category"
             target="#dropdown-example"
+            v-model='category'
+            placeholder='category'
+            item-text='name'
+            return-object
           ></v-overflow-btn>
         </v-flex>
-        <v-flex offset-md6 md3>
-          <v-btn
-            color='success'
-          >
-            ADD
-          </v-btn>
-        </v-flex>
 
+      </v-layout>
+      <v-layout>
+        <v-flex>
+            <v-flex>
+                <v-dialog v-model="dialog" persistent max-width="650">
+                  <v-btn slot="activator" color="success" style='width: 157%'>
+                    <v-icon>done_outline</v-icon>
+                  </v-btn>
+                  <v-card>
+                    <v-container>
+                      <v-layout row>
+                        <v-flex md7>
+                          <v-card-title class="headline">Add Product Confirmation</v-card-title>
+                          <v-card-text>Name: {{this.name}} </v-card-text>
+                          <v-card-text>Detail: {{this.detail}}</v-card-text>
+                          <v-card-text>Price: {{this.price}}</v-card-text>
+                          <v-card-text>Category: {{this.category.name}}</v-card-text>
+                        </v-flex>
+                        <v-flex md5 align-center justify-center>
+                          <img v-bind:src='img_url' alt='image' style='height: 40%' v-show="img_url.length != 0"/>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn @click="dialog = false">Disagree</v-btn>
+                      <v-btn color="success" @click='addProduct'>Confirm</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+            </v-flex>
+        </v-flex>
       </v-layout>
 
     </v-container>
   </v-app>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+import axios from '@/utils/axios'
 export default {
   name: 'addProduct',
   data: () => ({
-    categoryList: [
-      'cosmetic',
-      'device'
-    ],
-    imageUrl: '',
+    name: '',
+    detail: '',
+    img_url: '',
+    price: '',
+    category: {},
+    categories: [],
+    dialog: false,
     rules: {
       required: value => !!value || 'Required.',
-      counter: value => (value) ? value.length <= 255 || 'Max 255 characters' : null,
+      counter: value => (value) ? value.length <= 255 || 'Max 255 characters' : '',
       number: value => {
         const pattern = /(\d+\.)|(\d)/
         return pattern.test(value) || 'Invalid number'
       }
     }
-  })
+  }),
+  computed: {
+    ...mapGetters([
+      'getCategories'
+    ])
+  },
+  methods: {
+    async addProduct () {
+      const { data } = await axios.post('/product', {
+        name: this.name,
+        detail: this.detail,
+        img_url: this.img_url,
+        price: this.price,
+        category: this.category
+      })
+      console.log(data)
+      this.dialog = false
+      this.name = ''
+      this.detail = ''
+      this.img_url = ''
+      this.price = ''
+      this.category = ''
+    }
+  }
 }
 </script>
