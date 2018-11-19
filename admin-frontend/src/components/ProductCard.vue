0c9f9dd8b6f7e2eef5c8bf1a7c3e0d8f7c6836ee
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-19 19:55:33 +0700

    [UPDATE] admin-frontend / change layout of ProductCard

--- admin-frontend/src/components/ProductCard.vue
+++ admin-frontend/src/components/ProductCard.vue
@@ -1,54 +1,84 @@
 <template>
-    <v-card>
-      <v-img
-        v-bind:src='this.product.img_url'
-        aspect-ratio="2.75"
-      ></v-img>
-      <v-card-title primary-title>
-        <div>
-          <h3 class="headline mb-0">{{this.product.name}}</h3>
+  <v-layout align-center style='margin-bottom: 10px' column>
+    <v-card height='288' width='180'>
+      <v-layout style='height: 120px'>
+        <v-img
+          v-bind:src='this.product.img_url'
+          aspect-ratio="2.75"
+        ></v-img>
+      </v-layout>
+      <v-layout style='height: 75px' overflow-y-hidden>
+        <v-card-title primary-title>
+          <h6>
+            {{productNameFormat(this.product.name)}}
+          </h6>
+        </v-card-title>
+      </v-layout>
+      <v-layout style='height: 50'>
+        <v-card-actions >
+          <h3>{{product.price}} ฿</h3>
+        </v-card-actions>
+      </v-layout>
+      <v-layout style='height: 50' align-space-around fill-height>
+        <v-flex>
 
-        </div>
-      </v-card-title>
-      <v-card-actions>
-        <v-flex offset-md1>
-          <h2>{{product.price}} ฿</h2>
+            <v-dialog v-model="dialog" persistent max-width="290">
+              <v-btn slot="activator" color="error" style='width: 157%'>
+                <v-icon>delete_forever</v-icon>
+              </v-btn>
+              <v-card>
+                <v-card-title class="headline">Delete?</v-card-title>
+                <v-card-text>Do you want to delete {{product.name}} ?</v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn @click="dialog = false">Disagree</v-btn>
+                  <v-btn color="error" @click='deleteProduct(product.id)'>Delete</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+
         </v-flex>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" @click.native='addProductToRecommend(product, id)' >Add</v-btn>
-      </v-card-actions>
+      </v-layout>
     </v-card>
+  </v-layout>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import axios from '@/utils/axios'
 export default {
   name: 'productCard',
   props: [
     'product',
     'id'
   ],
+  data: () => ({
+    dialog: false
+  }),
   methods: {
     ...mapActions([
-      'setRecommendProducts',
-      'setProductUnRecommend',
-      'setProducts'
+      'setProducts',
+      'setDefaultProducts'
     ]),
-    addProductToRecommend (product, index) {
-      let temp = this.getProducts
-      const id = temp[index].id
-      temp.splice(index, 1)
-      let productLeft = this.getProductUnRecommend.filter(product => {
+    productNameFormat (name) {
+      if (name.length > 40) {
+        return name.substring(0, 35) + '...'
+      }
+      return name
+    },
+    async deleteProduct (id) {
+      const { data } = await axios.delete(`/product/${id}`)
+      console.log(data)
+      const products = this.getProducts.filter(product => {
         return product.id != id
       })
-      this.setProductUnRecommend(productLeft)
-      this.setRecommendProducts(product)
+      this.setProducts(products)
+      this.setDefaultProducts(products)
+      this.dialog = false
     }
   },
   computed: {
     ...mapGetters([
-      'getRecommendProducts',
-      'getProducts',
-      'getProductUnRecommend'
+      'getProducts'
     ])
   }
 
