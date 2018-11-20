77ada3187f0ec64fdc113c7d6b18577cd92e7408
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-20 17:13:40 +0700

    [ADD] frontend / add SummaryOrder

---
+++ frontend/src/components/SummaryOrder.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <div class="columns">
+      <div class="column is-offset-1 is-5">
+        <span class="title is-3">Order Detail:</span>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-offset-1">
+        <div class="box">
+          <div class="columns">
+            <div class="column">
+              Order #1
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              Place on 10 / 10 / 2018
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-offset-1">
+        <div class="box">
+          <div class="columns">
+            <div class="column is-2">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS459I2QXTcVBgxxy5mWSqCpvNqm9XvtydEnayb7yrpusARcARiVQ" alt="img" />
+            </div>
+            <div class="column is-10">
+              <div class="columns">
+                <div class="column is-12">
+                  <span class="title is-4">
+                    iPhone X 64 GB RED Product
+                  </span>
+                </div>
+              </div>
+              <div class="columns" style="height: 150px">
+                <div class="column is-12">
+                  <span class="title is-6">
+                    126 King Mongkut's
+                  </span>
+
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column is-offset-8 is-2">
+                  <span class="title is-4">
+                    100 x 5
+                  </span>
+                </div>
+                <div class="column is-2">
+                  <span class="title is-4">
+                    500 ฿
+                  </span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-offset-10 is-2">
+        <a class="button is-primary">ช๊อปต่อเลย</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SummaryOrder'
+}
+</script>
