d0b71b5409518dc7ed616f5742009aa4d3d079f7
Grizzly Bear <vitsarut.udomphol2@mail.kmutt.ac.th>
2018-11-20 17:03:25 +0700

    [UPDATE] frontend / add /summary route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/components/Home'
 import ProductDetail from '@/components/ProductDetail'
 import CategoryProduct from '@/components/CategoryProduct'
 import SearchPage from '@/components/SearchPage'
+import SummaryOrder from '@/components/SummaryOrder'
 
 Vue.use(Router)
 
@@ -35,6 +36,11 @@ export default new Router({
       path: '/search',
       name: 'SearchProduct',
       component: SearchPage
+    },
+    {
+      path: '/summary',
+      name: 'SummaryOrder',
+      component: SummaryOrder
     }
   ]
 })
