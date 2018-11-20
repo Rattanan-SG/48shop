8b7c5e16642ffdf8fa815be33d42996400ae7572
rattanan.nan@mail.kmutt.ac.th <rattanan.nan@mail.kmutt.ac.th>
2018-11-20 19:20:08 +0700

    [FIX] UserProfile can CURD

--- backend/src/main/java/com/sit/int202/backend/UserProfile/UserProfile.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/UserProfile.java
@@ -2,30 +2,23 @@ package com.sit.int202.backend.UserProfile;
 
 import java.io.Serializable;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.sit.int202.backend.Address.Address;
-import com.sit.int202.backend.Order.Order;
-
 @Entity
 @Table(name = "user_profiles")
 @EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class UserProfile implements Serializable {
 
     @Id
@@ -41,16 +34,6 @@ public class UserProfile implements Serializable {
 
     @NotBlank
     private String lastname;
-
-    @OneToMany(mappedBy = "userProfile", fetch = FetchType.LAZY)
-    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
-    @JsonIgnore
-    private List<Address> addresses;
-
-    @OneToMany(mappedBy = "userProfile", fetch = FetchType.LAZY)
-    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
-    @JsonIgnore
-    private List<Order> orders;
 
     public UserProfile() {
     }
@@ -79,20 +62,12 @@ public class UserProfile implements Serializable {
         this.lastname = lastname;
     }
 
-    public List<Address> getAddress() {
-        return this.addresses;
+    public String getFacebookId() {
+        return facebookId;
     }
 
-    public void setAddresses(List<Address> addresses) {
-        this.addresses = addresses;
-    }
-
-    public List<Order> getOrders() {
-        return this.orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
+    public void setFacebookId(String facebookId) {
+        this.facebookId = facebookId;
     }
 
 }
--- backend/src/main/java/com/sit/int202/backend/UserProfile/UserProfileService.java
+++ backend/src/main/java/com/sit/int202/backend/UserProfile/UserProfileService.java
@@ -16,6 +16,7 @@ import co.omise.models.OmiseException;
 
 @Service
 public class UserProfileService {
+
     Properties prop = new Properties();
 
     @Autowired
@@ -38,27 +39,27 @@ public class UserProfileService {
         return id;
     }
 
-    public String createCustomerToken(LinkedHashMap userData) throws IOException, ClientException, OmiseException{
+    public String createCustomerToken(LinkedHashMap userData) throws IOException, ClientException, OmiseException {
         prop.load(this.getClass().getResourceAsStream("/application.properties"));
         final String PUBLIC_KEY = prop.getProperty("PUBLIC_KEY");
         final String SECRET_KEY = prop.getProperty("SECRET_KEY");
 
-        Client client = new Client(PUBLIC_KEY,SECRET_KEY);
+        Client client = new Client(PUBLIC_KEY, SECRET_KEY);
         Customer customer = client.customers().create(
-            new Customer.Create()
-            .description(userData.get("first_name").toString()+" "+userData.get("last_name").toString())
+                new Customer.Create()
+                        .description(userData.get("first_name").toString() + " " + userData.get("last_name").toString())
         );
         return customer.getId();
     }
 
-    public void updateCard(String token,String customerToken) throws IOException, ClientException, OmiseException {
+    public void updateCard(String token, String customerToken) throws IOException, ClientException, OmiseException {
         prop.load(this.getClass().getResourceAsStream("/application.properties"));
         final String PUBLIC_KEY = prop.getProperty("PUBLIC_KEY");
         final String SECRET_KEY = prop.getProperty("SECRET_KEY");
 
-        Client client = new Client(PUBLIC_KEY,SECRET_KEY);
+        Client client = new Client(PUBLIC_KEY, SECRET_KEY);
         Customer customer = client.customers().update(
-            customerToken,new Customer.Update().card(token)
+                customerToken, new Customer.Update().card(token)
         );
     }
 
